2c73337afbdb65b805fa7328dec2576b091d5dbc
Edson Joao Andrés Pereira Alvarado <epereira-2021576@kinal.edu.gt>
2024-01-24 17:17:00 -0600

    Discarding logic

--- assets/scripts/logicIndex.js
+++ assets/scripts/logicIndex.js
@@ -1,11 +1,1 @@
-const links = document.querySelectorAll('.nav-link');
 
-links.forEach((link) => {
-  link.addEventListener('click', async (event) => {
-    alert('Yendo al documento...');
-
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
-    window.location.href = event.target.getAttribute('href');
-  });
-});
