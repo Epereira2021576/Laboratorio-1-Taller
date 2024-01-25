fa17634b6fc6d8c021ae500f44a602e310f36e79
Edson Joao Andrés Pereira Alvarado <epereira-2021576@kinal.edu.gt>
2024-01-25 10:30:27 -0600

    Finished the function related to background music

--- assets/scripts/logicPortfolio.js
+++ assets/scripts/logicPortfolio.js
@@ -1,21 +1,20 @@
-const sonido = document.getElementById('musicaFondo');
-const botonMute = document.getElementById('botonMute');
-const imagenMute = document.getElementById('imagenMute');
-const imagenUnmute = document.getElementById('imagenUnmute');
-const textoMute = document.getElementById('textoMute');
+document.addEventListener('DOMContentLoaded', () => {
+  const sonido = document.getElementById('musicaFondo');
+  const botonMute = document.getElementById('botonMute');
+  const imagenMute = document.getElementById('imagenMute');
+  const imagenUnmute = document.getElementById('imagenUnmute');
 
-botonMute.addEventListener('click', function () {
-  if (sonido.muted) {
-    sonido.muted = false;
-    imagenMute.style.display = 'block';
-    imagenUnmute.style.display = 'none';
-    textoMute.textContent = 'Mute';
-    botonMute.setAttribute('aria-label', 'Mute');
-  } else {
-    sonido.muted = true;
-    imagenMute.style.display = 'none';
-    imagenUnmute.style.display = 'block';
-    textoMute.textContent = 'Unmute';
-    botonMute.setAttribute('aria-label', 'Mute');
-  }
+  botonMute.addEventListener('click', function () {
+    if (sonido.muted) {
+      sonido.muted = false;
+      imagenMute.style.display = 'block';
+      imagenUnmute.style.display = 'none';
+      botonMute.setAttribute('aria-label', 'Unmute');
+    } else {
+      sonido.muted = true;
+      imagenMute.style.display = 'none';
+      imagenUnmute.style.display = 'block';
+      botonMute.setAttribute('aria-label', 'Mute');
+    }
+  });
 });
