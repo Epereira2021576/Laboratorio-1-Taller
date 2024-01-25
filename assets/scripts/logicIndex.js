dbd2bdc9208fdad621ea8aa53e2bc9f496863426
Edson Joao Andrés Pereira Alvarado <epereira-2021576@kinal.edu.gt>
2024-01-25 17:44:47 -0600

    Began adding logic on logicIndex.js

--- assets/scripts/logicIndex.js
+++ assets/scripts/logicIndex.js
@@ -1,1 +1,10 @@
+function navigateToLink(linkElement) {
+  const filename = linkElement
+    .getAttribute('href')
+    .split('/')
+    .pop()
+    .split('.')[0];
 
+  const message = `Nos vamos a ${filename}`;
+  window.location.href = linkElement.getAttribute('href');
+}
